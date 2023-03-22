c8f27d73948f8a30227360cf67c3d6ac5f2fcd9d
Will Yeadon <chvyalthan@gmail.com>
2023-03-22 12:50:18 +0000

    script

--- Response script.py
+++ Response script.py
@@ -1,1 +1,43 @@
-pass
+import openai
+import openpyxl
+from openpyxl import load_workbook
+
+# Set up the OpenAI API
+openai.api_key = "Use your own!"
+
+# Load the Excel workbook
+workbook = load_workbook("Question-Answer-Bank.xlsx")
+worksheet = workbook.active
+
+# Define the prompt prefix
+prompt_prefix = "Answer the following question as correctly and concisely as possible ideally using numbers alone including units where appropriate: '"
+
+prompt_suffix = "'? Please answer below."
+
+# Iterate through the rows, starting from row 2 (skipping the header)
+for row in range(2, worksheet.max_row + 1):
+    question = worksheet.cell(row=row, column=6).value  # Column 6 is the 'Question' column
+
+    if question is not None:
+        prompt = prompt_prefix + question + prompt_suffix
+
+        # Call the OpenAI API with the prompt
+        response = openai.Completion.create(
+            engine="text-davinci-002",
+            prompt=prompt,
+            max_tokens=50,
+            n=1,
+            stop=None,
+            temperature=0.5,
+        )
+
+        # Extract the answer from the API response
+        answer = response.choices[0].text.strip()
+
+        # Store the answer in the 'Response' column (column 8)
+        worksheet.cell(row=row, column=8).value = answer
+
+        # Rinse and repeat for the different models
+
+# Save the updated workbook
+workbook.save("Question-Answer-Bank-Updated.xlsx")
